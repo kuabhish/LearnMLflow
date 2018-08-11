44049de2edc6cb0528ea28f88c70235e82a38b4b
Abhishek Kumar <abhi.vickycool@gmail.com>
2018-08-12 01:32:17 +0530

    Update try.py

--- try.py
+++ try.py
@@ -3,7 +3,7 @@ import sys
 
 with mlflow.start_run():
     if(sys.argv[1]):
-        alpha = sys.argv[1]
+        alpha = float(sys.argv[1])
     else:
         alpha = 1
     mlflow.log_param('alpha', alpha)
